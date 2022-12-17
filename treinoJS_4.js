fb5b49f39e898576627acaeee642cb6ebf55892f
Mateus <108558575+MateusToniason@users.noreply.github.com>
2022-12-17 16:09:37 -0300

    Update treinoJS_4.js

--- treinoJS_4.js
+++ treinoJS_4.js
@@ -1,4 +1,3 @@
 /* declarando "confirm" */
-var nome = prompt('Digite seu nome:')
-alert('Olá ' + nome + ', meu nome é JavaScript, seja bem vindo a página inicial!')
-
+var nome = prompt('Digite seu nome:');
+alert('Olá ' + nome + ', meu nome é JavaScript, seja bem vindo a página inicial!');
